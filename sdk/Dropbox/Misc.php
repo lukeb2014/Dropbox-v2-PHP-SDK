6891aca2980b99579f9726f436278af8cfbad5bf
nicolaszanghi <nicolas.zanghi@gmail.com>
2018-01-29 17:12:50 +0100

    debug class Entry
    When I tried to use upload_session_start, upload_session_append_v2 and upload_session_finish. I find some bugs.

--- sdk/Dropbox/Misc.php
+++ sdk/Dropbox/Misc.php
@@ -34,11 +34,11 @@
         public $commit;
         
         public function __construct($session_id, $offset, $path, $mode = 'add', $autorename = false, $mute = false) {
-            $cursor = array(
-                "sesson_id" => $session_id,
+            $this->cursor = array(
+                "session_id" => $session_id,
                 "offset" => $offset
             );
-            $commit = array(
+            $this->commit = array(
                 "path" => $path,
                 "mode" => $mode,
                 "autorename" => $autorename,
@@ -48,8 +48,8 @@
         
         public function toJson() {
             return json_encode(array(
-                "cursor" => $cursor,
-                "commit" => $commit
+                "cursor" => $this->cursor,
+                "commit" => $this->commit
             ));
         }
     }
